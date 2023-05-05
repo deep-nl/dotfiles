a613877a56238275e4510117ed01ac7e07a28fae
deep-nl <nilei1797@gmail.com>
2023-05-05 21:14:34 +0800

    add my sh files3

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,4 +1,6 @@
 apt remove neovim
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz nvim && rm -rf nvim-linux64.tar.gz
+mkdir -p ~/nvim 
+tar xzvf nvim-linux64.tar.gz -C ~/nvim 
+rm -rf nvim-linux64.tar.gz
 
